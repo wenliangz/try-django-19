df98f33a01e28404dfb10100d3d0446729924204
Wenliang Zhang <wzhang@bluebirdbio.com>
2016-08-30 13:53:13 -0400

    8 - Customize Admin -wen

--- src/posts/models.py
+++ src/posts/models.py
@@ -6,7 +6,7 @@ class Post(models.Model):
     title = models.CharField(max_length=120)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
-    udpated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
 
     def __unicode__(self):
         return self.title
